b28d461648677a846285b57cfd6904df2d49c877
takahashi <jin_25s1100444@nnn.ed.jp>
2025-08-05 17:59:54 +0900

    [BE]create comment model and serializers

--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import Shop , ShopImage
+from django.contrib.auth.models import User
+from .models import Comment
 
 class ShopSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +12,17 @@ class ShopImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShopImage
         fields =' __all__ '
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    account = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'title', 'content', 'image', 'ratingStar', 'account']
