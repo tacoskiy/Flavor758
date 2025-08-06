1d6917590b060ce366f02398a8bdf51cd649c4ad
YUSEMAN <authority1001x@gmail.com>
2025-08-07 00:07:58 +0900

    [BE] create shopName filter

--- backend/app/filters.py
+++ backend/app/filters.py
@@ -2,9 +2,10 @@ import django_filters # type: ignore
 from .models import Shop
 
 class ShopFilter(django_filters.FilterSet):
+    shopName = django_filters.CharFilter(field_name="name", lookup_expr='icontains')
     min_rating = django_filters.NumberFilter(field_name="rating", lookup_expr='gte')
     category = django_filters.CharFilter(lookup_expr='icontains')
 
     class Meta:
         model = Shop
-        fields = ['category', 'min_rating']
+        fields = ['shopName','category', 'min_rating']
